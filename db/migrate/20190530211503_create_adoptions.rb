977a3f8f06a5d280ba01b314c840a3f5c8ee44a1
Redmond Chan <redmondchan92@gmail.com>
2019-05-30 17:40:27 -0400

    added table columns

--- db/migrate/20190530211503_create_adoptions.rb
+++ db/migrate/20190530211503_create_adoptions.rb
@@ -1,7 +1,6 @@
 class CreateAdoptions < ActiveRecord::Migration[5.2]
   def change
     create_table :adoptions do |t|
-
       t.timestamps
     end
   end
--- db/migrate/20190530211918_create_dogs.rb
+++ db/migrate/20190530211918_create_dogs.rb
@@ -1,7 +1,11 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
-
+      t.string :location
+      t.string :name
+      t.string :breed
+      t.string :status
+      t.string :image_url
       t.timestamps
     end
   end

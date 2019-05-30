9c07e79350a7322c55e0612b911899a1d5a557c5
Redmond Chan <redmondchan92@gmail.com>
2019-05-30 19:25:26 -0400

    created actions in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    redirect_to "/sessions/new" unless logged_in?
+  end
+
+  def index
+    current_user
+  end
+
 end

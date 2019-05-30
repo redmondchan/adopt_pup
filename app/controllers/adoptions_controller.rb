47006e6507f8227e8ecd9855f2d02bb7bb23d67e
Redmond Chan <redmondchan92@gmail.com>
2019-05-30 18:08:30 -0400

    created actions in adoptions controller

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,24 @@
 class AdoptionsController < ApplicationController
+  def index
+    @adoptions = Adoptions.all
+    render :index
+  end
+
+  def new
+    @adoption = Adoption.new
+  end
+
+  def create
+    @adoption = Adoption.create(adoption_params)
+    @adoption.dog.status = "Adopted by #{@adoption.user.username}"
+    @adoption.dog.save
+    redirect_to dog_path(@adoption.dog)
+  end
+
+  private
+
+  def adoption_params
+    params.require(:adoption).permit(:user_id, :dog_id)
+  end
+  
 end

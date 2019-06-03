4eed55480c786e79013768d760dde353eb3616d9
Redmond Chan <redmondchan92@gmail.com>
2019-06-03 16:26:01 -0400

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :dogs
-  resources :adoptions
+  get "/sessions", to: "sessions#index"
+  get "/sessions/new", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/", to: "application#index"
+  resources :users, only: [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :dogs, only: [:index, :new, :show, :create, :edit, :update]
+  resources :adoptions, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Dog.create(location: "New York", name: "Maddy", breed: "Husky", status: "Not Adopted", image_url: "https://media1.tenor.com/images/eaccdc8a3ed89ae8385579ea03223417/tenor.gif?itemid=5397431")
+Dog.create(location:"Chicago",name: "Sunny", breed: "German Shepard", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg" )
+Dog.create(location: "New York", name: "Cheesecake", breed:"Husky", status: "Not Adopted", image_url: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Black-Magic-Big-Boy.jpg" )
+Dog.create(location: "Miami", name: "Yuyi", breed:"Pitbull", status: "Not Adopted", image_url: "https://815678169699-bfas-files.s3-us-west-2.amazonaws.com/Roxy-pit-bull-terrier.jpg" )
+Dog.create(location: "Los Angeles", name: "Tony", breed:"Pitbull", status: "Not Adopted", image_url: "https://815678169699-bfas-files.s3-us-west-2.amazonaws.com/Roxy-pit-bull-terrier.jpg" )
+
+Dog.create(location:"Hong Kong", name: "Luther", breed: "Affenpinscher", status: "Not Adopted", image_url: "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Affenpinscher-1.jpg")
+Dog.create(location:"Paris", name: "Pikachu", breed: "Australian Shepherd", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/03173132/Australian-Shepherd.1.jpg")
+Dog.create(location:"Toronto", name: "Lucky", breed: "Bichon Frise", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13000402/Bichon-Frise-On-White-03.jpg")
+Dog.create(location:"Milan", name: "Justice", breed: "Bulldog", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/21010129/Bulldog-Slide-02.jpg")
+Dog.create(location:"New Zealand", name: "Loki", breed: "Corgi", status: "Not Adopted", image_url: "https://ih1.redbubble.net/image.439646371.5865/flat,550x550,075,f.u1.jpg")
+Dog.create(location:"New Zealand", name: "Rex", breed: "Corgi", status: "Not Adopted", image_url: "https://media2.giphy.com/media/1hpRkVM1XQgOQ/giphy.gif")
+Dog.create(location:"Buenos Aires", name: "Bruno", breed: "Chow Chow", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234523/Chow-Chow-On-White-01.jpg")
+Dog.create(location:"Tokyo", name: "Sammy", breed: "Samoyed", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/31120855/515865991-800x600.jpg")
+Dog.create(location:"Oregon", name: "Ghost", breed: "Samoyed", status: "Not Adopted", image_url: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224733/Samoyed-MP.jpg")
+Dog.create(location:"Boston", name: "Bumble", breed: "Golden Retriever", status: "Not Adopted", image_url: "https://www.grca.org/wp-content/uploads/2015/08/puppyflower.jpg")
+Dog.create(location:"Florida", name: "Fido", breed: "Labrador", status: "Not Adopted", image_url: "https://www.balfourcanada.com/wp-content/uploads/2017/10/11.jpg")
+Dog.create(location:"Osaka", name: "Eagle", breed: "Beagle", status: "Not Adopted", image_url: "https://www.dogmal.com/wp-content/uploads/2016/10/beagle-dog-breed.jpg")
+Dog.create(location:"Kyoto", name: "Poo Poo", breed: "Poodle", status: "Not Adopted", image_url: "https://s3.us-west-2.amazonaws.com/photos.puppyspot.com/breeds/322/card/500000295_medium.jpg")
+
+Dog.create(location:"Kanto", name: "Flareon", breed: "Pokemon", status: "Not Adopted", image_url: "https://media1.tenor.com/images/033b4b302327bb91bca911f5cbe371b2/tenor.gif?itemid=7749568")
+Dog.create(location:"Kanto", name: "Jolteon", breed: "Pokemon", status: "Not Adopted", image_url: "https://i.pinimg.com/originals/63/2c/19/632c1908092972bccbeb211fa9ca93e6.gif")
+Dog.create(location:"Kanto", name: "Vaporeon", breed: "Pokemon", status: "Not Adopted", image_url: "https://i.pinimg.com/originals/7a/8d/79/7a8d79195729f05e6c0f13a7cf3b10ee.gif")
+
+User.create(username:"red", password:"1")
+User.create(username:"rosanna", password:"1")
